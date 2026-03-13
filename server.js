9460bcf4d2e203592d0feecc6a57849b2e382585
XxX9Void9XxX <brooksm@carbonschools.org>
2026-03-13 15:37:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 import express from "express";
 import http from "http";
-import { createBareServer } from "@mercuryworkshop/bare-server-node";
+import { createBareServer } from "bare-server-node";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Create the bare server
 const bare = createBareServer("/bare/");
 
+// Serve static files
 app.use(express.static("public"));
 
 const server = http.createServer(app);
 
+// Route HTTP requests
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
     bare.routeRequest(req, res);
   }
 });
 
+// Handle websocket upgrades (needed for some proxy traffic)
 server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req)) {
     bare.routeUpgrade(req, socket, head);
   }
 });
 
+// Start server
 server.listen(port, () => {
   console.log("Server running on port " + port);
 });
